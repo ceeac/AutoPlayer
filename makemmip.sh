9fb0a1102295688f4562aca17d55733f211c152a
ceeac <ceeac@users.noreply.github.com>
2016-07-28 20:21:34 +0200

    Include version number into mmip script.

--- makemmip.sh
+++ makemmip.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+version="0.1.0"
 
 rm -f $(ls | grep "\.mmip$")
-"/c/Program Files/7-Zip/7z.exe" a -tzip -- AutoPlayer.mmip $(find . -type f -name "*.vbs" -o -name "*.ini")
+"/c/Program Files/7-Zip/7z.exe" a -tzip -- AutoPlayer-$version.mmip $(find . -type f -name "*.vbs" -o -name "*.ini")
